ed2f3bf56bc3b0b52b7b9094986fe09da8cdcc65
Kazuhiro Uchikata <kazuuchikata@gmail.com>
2016-12-05 18:17:20 -0600

    added Steve Jobs Dummy

--- static/code.js
+++ static/code.js
@@ -12,81 +12,69 @@ var cy = cytoscape({
   style: cytoscape.stylesheet()
     .selector('node')
       .css({
-        'height': 80,
-        'width': 80,
+        'height': 'data(popularity)',
+        'width': 'data(popularity)',
         'background-fit': 'cover',
         'border-color': '#000',
         'border-width': 3,
         'border-opacity': 0.5
       })
-    .selector('.eating')
-      .css({
-        'border-color': 'red'
-      })
-    .selector('.eater')
-      .css({
-        'border-width': 9
-      })
     .selector('edge')
       .css({
         'width': 6,
         'target-arrow-shape': 'triangle',
-        'line-color': '#ffaaaa',
-        'target-arrow-color': '#ffaaaa',
+        'line-color': 'data(faveColor)',
+        'target-arrow-color': 'data(faveColor)',
         'curve-style': 'bezier'
       })
-    .selector('#bird')
+    .selector('#Steve')
       .css({
-        'background-image': 'https://farm8.staticflickr.com/7272/7633179468_3e19e45a0c_b.jpg'
+        'background-image': 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg'
       })
-    .selector('#cat')
+    .selector('#Laurene')
       .css({
-        'background-image': 'https://farm2.staticflickr.com/1261/1413379559_412a540d29_b.jpg'
+        'background-image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Laurene_Powell_Jobs.jpg/220px-Laurene_Powell_Jobs.jpg'
       })
-    .selector('#ladybug')
+    .selector('#Chrisann')
       .css({
-        'background-image': 'https://farm4.staticflickr.com/3063/2751740612_af11fb090b_b.jpg'
+        'background-image': 'http://i.dailymail.co.uk/i/pix/2015/08/07/16/2B2E51CA00000578-3188738-image-m-35_1438959595262.jpg'
       })
-  .selector('#aphid')
+  .selector('#Reed')
       .css({
-        'background-image': 'https://farm9.staticflickr.com/8316/8003798443_32d01257c8_b.jpg'
+        'background-image': 'http://enewsdaily.info/now/wp-content/uploads/2012/09/Reed-Jobs-Powell-Steve-Jobs-son_pic_thumb.jpg'
       })
-  .selector('#rose')
+  .selector('#Eve')
       .css({
-        'background-image': 'https://farm6.staticflickr.com/5109/5817854163_eaccd688f5_b.jpg'
+        'background-image': 'http://thedailytruffle.com/wp-content/uploads/2015/10/Eve-Jobs-dress.jpg'
       })
-  .selector('#grasshopper')
+  .selector('#Lisa')
       .css({
-        'background-image': 'https://farm7.staticflickr.com/6098/6224655456_f4c3c98589_b.jpg'
-      })
-  .selector('#plant')
-      .css({
-        'background-image': 'https://farm1.staticflickr.com/231/524893064_f49a4d1d10_z.jpg'
-      })
-  .selector('#wheat')
-      .css({
-        'background-image': 'https://farm3.staticflickr.com/2660/3715569167_7e978e8319_b.jpg'
+        'background-image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Lisa_Brennan-Jobs.jpg/220px-Lisa_Brennan-Jobs.jpg'
       }),
   
   elements: {
     nodes: [
-      { data: { id: 'cat' } },
-      { data: { id: 'bird' } },
-      { data: { id: 'ladybug' } },
-      { data: { id: 'aphid' } },
-      { data: { id: 'rose' } },
-      { data: { id: 'grasshopper' } },
-      { data: { id: 'plant' } },
-      { data: { id: 'wheat' } }
+      { data: { id: 'Steve', name: 'Steve Jobs' , birth: 'February 24, 1955', death: 'October 5, 2011', alma: 'Reed College', popularity: '300'} },
+      { data: { id: 'Laurene', name: 'Laurene Powell', birth: 'November 6, 1963', death: 'N/A', alma: 'Stanford University', popularity: '100' } },
+      { data: { id: 'Chrisann', name: 'Steve Jobs' , birth: 'September 29, 1954', death: 'N/A', alma: ' ', popularity: '50'} },
+      { data: { id: 'Reed', name: 'Reed Jobs' , birth: 'N/A', death: 'N/A', alma: 'N/A', popularity: '50'} },
+      { data: { id: 'Eve', name: 'Eve Jobs' , birth: 'N/A', death: 'N/A', alma: 'N/A', popularity: '50' } },
+      { data: { id: 'Lisa', name: 'Lisa Brennan-Jobs' , birth: 'May 17, 1978', death: 'N/A', alma: 'Harvard University', popularity: '150' } },
+      { data: { id: 'Erin', name: 'Erin Jobs' , birth: 'N/A', death: 'N/A', alma: 'N/A', popularity: '50'} },
+      { data: { id: 'Paul', name: 'Paul Jobs' , birth: 'N/A', death: 'N/A', alma: 'N/A', popularity: '50' } },
+      { data: { id: 'Clara', name: 'Clara Jobs' , birth: 'N/A', death: 'N/A', alma: 'N/A', popularity: '50' } },
     ],
     edges: [
-      { data: { source: 'cat', target: 'bird' } },
-      { data: { source: 'bird', target: 'ladybug' } },
-      { data: { source: 'bird', target: 'grasshopper' } },
-      { data: { source: 'grasshopper', target: 'plant' } },
-      { data: { source: 'grasshopper', target: 'wheat' } },
-      { data: { source: 'ladybug', target: 'aphid' } },
-      { data: { source: 'aphid', target: 'rose' } }
+      { data: { source: 'Steve', target: 'Reed' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Steve', target: 'Eve' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Steve', target: 'Reed' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Steve', target: 'Eve' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Steve', target: 'Lisa' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Steve', target: 'Erin' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Laurene', target: 'Reed' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Laurene', target: 'Lisa' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Laurene', target: 'Eve' , faveColor: '#ffaaaa'} },
+      { data: { source: 'Chrisann', target: 'Erin' , faveColor: '#ffaaaa'} }
     ]
   },
   
@@ -97,10 +85,9 @@ var cy = cytoscape({
   }
 }); // cy init
   cy.nodes().forEach(function(n){
-    var g = n.data('id');
 
     n.qtip({
-      content: g,
+      content: {title: n.data('name'), text: "Birth Date: " + n.data('birth') + "\n" + "Death Date: " + n.data('death') + "\n" + "Alma Mater: " + n.data('alma') + "\n" + "Popularity Score: " + n.data('popularity')},
       position: {
         my: 'top left',
         at: 'right center'
@@ -109,13 +96,38 @@ var cy = cytoscape({
         classes: 'qtip-bootstrap',
         tip: {
           width: 16,
-          height: 8
+          height: 8,
         } 
       }
     });
   });
 
+  cy.add({
+    group: "edges",
+    data: { source: 'Steve', target: 'Laurene' , faveColor: '#aaffaa'}
+  });
 
+  cy.add({
+    group: "edges",
+    data: { source: 'Steve', target: 'Chrisann' , faveColor: '#aaffaa'}
+  });
 
+  cy.add({
+    data: { source: 'Steve', target: 'Clara' , faveColor: '#aaaaff'}
+  });
+
+  cy.add({
+    data: { source: 'Steve', target: 'Paul' , faveColor: '#aaaaff'}
+  });
+
+  cy.$('#Clara').position({
+  x: cy.$('#Steve').position('x') - 200,
+  y: cy.$('#Steve').position('y') - 400
+});
+
+  cy.$('#Paul').position({
+  x: cy.$('#Steve').position('x') + 200,
+  y: cy.$('#Steve').position('y') - 400
+});
 
 }); // on dom ready
